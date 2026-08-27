0632fda42a299363a4489454a059bcca09c49887
ranveervatsya837-commits <Ranveervatsya837@gmail.com>
2026-08-27 21:38:52 +0530

    Fix typo to correct model llama-3.1-8b-instant

--- main.py
+++ main.py
@@ -305,7 +305,7 @@ Question:
 
         try:
             response = client.chat.completions.create(
-                model="llama-3.3-70b-versatile",
+                model="llama-3.1-8b-instant",
                 messages=messages
             )
             return response.choices[0].message.content
